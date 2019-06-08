5879d41d371ea7f13999a94d1811fc5831899db1
Dallen R. Cluff <49734187+Cluff-Dallen@users.noreply.github.com>
2019-06-08 12:57:29 -0600

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -21,18 +21,42 @@ using namespace std;
  ***********************************************/
 void fibonacci()
 {
-   // show the first serveral Fibonacci numbers
-   int number;
+    int number;
    cout << "How many Fibonacci numbers would you like to see? ";
    cin  >> number;
 
+   long int first = 0;
+   long int second = 1;
+   long int temp;
+   cout << 1 << endl;
    // your code to display the first <number> Fibonacci numbers
+   for (int i = 1; i < number; i++)
+   {
+      temp = second;           
+      second += first;          
+      first = temp;            
 
-   // prompt for a single large Fibonacci
+      cout << second << ",";
+   }
+   
+   cout << endl;
+   
+    // prompt for a single large Fibonacci
    cout << "Which Fibonacci number would you like to display? ";
    cin  >> number;
-
-   // your code to display the <number>th Fibonacci number
+   
+   first = 0;
+   second = 1;
+   //does not handle very large numbers such as fib #100.
+   for (int i = 1; i < number; i++)
+   {
+      temp = second;             //Temp Holder
+      second += first;          //Add the two Fibonacci numbers
+      first = temp;             //increment up
+      
+      if (i == number - 1)
+      cout << second << endl;
+   }
 }
 
 
